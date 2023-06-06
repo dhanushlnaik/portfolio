3ad94a95d92c5306906e7d045b3a63474051ca79
Dhanush Naik <dhanushlnaik@gmail.com>
2023-06-06 23:57:56 +0530

    🌸 : Tailwind Configuration and some Boilercode

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from './Logo'
+
+const Navbar = () => {
+  return (
+    
+    <div className='w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4'>
+        <Logo/>
+    </div>
+  )
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    maxWidth: {
+      container: "1440px",
+      contentContainer: "1140px",
+      containerSmall: "1024px",
+      containerxs: "768px"
+    },
     extend: {
+      screens: {
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px",
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -15,6 +31,16 @@ module.exports = {
       fontFamily: {
         bodyFont: ["Montserrat", "sans-serif"],
         titleFont: ["Inter", "sans-serif"]
+      },
+      boxShadow: {
+        navbarShadow: "0 10px 30ps -10px rgba(2,12,27,0.7)",
+      },
+      colors: {
+        bodyColor: "#0A192F",
+        textC: "#64ffda",
+        textDark: "#8892b0",
+        textLight: "ccd6f6",
+        hoverColor: "rgba(100,255,218,0.1)",
       }
     },
   },
