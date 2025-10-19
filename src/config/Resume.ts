1ad9a950d1e74e72501a26a558ee4e0826e75e95
Dhanush Naik <77430067+dhanushlnaik@users.noreply.github.com>
2025-10-19 17:34:03 +0530

    Update resumeConfig URL to new Google Drive link

--- src/config/Resume.ts
+++ src/config/Resume.ts
@@ -1,3 +1,3 @@
 export const resumeConfig = {
-  url: 'https://drive.google.com/file/d/1ormIiMVpWGAMOZ3FZVj_XrKPkEmPlPQj/preview',
+  url: 'https://drive.google.com/drive/folders/1SWHY_7QBvedQsQzUvC4QMker1btwnaBW/preview',
 };
